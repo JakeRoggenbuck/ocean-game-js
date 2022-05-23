62399901934834d7bc6dd578304df02f62d90643
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-05-23 10:47:06 -0700

    Add index and main

---
+++ main.js
@@ -0,0 +1,13 @@
+const DARK_BLUE = '#1D3557';
+const MID_BLUE = '#457B9D';
+const LIGHT_BLUE = '#A8DADC';
+const WHITE = '#F1FAEE';
+const RED = '#E63946';
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(DARK_BLUE);
+}
