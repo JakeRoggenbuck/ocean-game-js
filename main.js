b4b600a5749b5aa08dbb6760fcf40051b9a7fbd3
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-05-23 10:59:17 -0700

    Add more

--- main.js
+++ main.js
@@ -4,10 +4,36 @@ const LIGHT_BLUE = '#A8DADC';
 const WHITE = '#F1FAEE';
 const RED = '#E63946';
 
+let x = 100;
+let y = 100;
+let angle = 0.0;
+let seg_length = 50;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  strokeWeight(20.0);
+  stroke(255, 100);
 }
 
 function draw() {
   background(DARK_BLUE);
+
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    y -= 5;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    y += 5;
+  }
+
+  ellipse(x, y, 20, 20);
+
 }
